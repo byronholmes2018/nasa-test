067d3357b42af11ce7d4e710c828d1b58bfbe39e
ByronHolmes-at-129020800614 <lshopeful2018@gmail.com>
2022-01-31 21:36:23 -0600

    Changes

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
+const dataHolder = document.querySelector('.data-holder');
+
 const getAsteroid = async () => {
-  console.log('get asteroid');
-  const response = await fetch(
-    'https://ssd-api.jpl.nasa.gov/sbdb.api?des=73P&alt-des=1&cd-epoch=1',
-    {
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET',
-      },
-    }
-  );
+  const response = await fetch('http://localhost:3000/smallBody');
   console.log(response);
   const data = await response.json();
   console.log(data);
+  return data;
+};
+
+const postAsteroid = async () => {
+  const response = await fetch('http://localhost:3000/testPost', {
+    method: 'post',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ myKey: 'myVal' }),
+  });
+
+  const data = await response.json();
+  return data;
 };
 
 console.log('changes');
 
-getAsteroid()
-  .then((data) => console.log('data', data))
-  .catch((err) => console.log('err ', err));
+// getAsteroid()
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log('err ', err));
+
+postAsteroid()
+  .then((data) => console.log('response from server, data', data))
+  .catch((err) => console.log('error ', err));
